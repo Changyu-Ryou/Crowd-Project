26eb1073c73cc089e28ec82bbbd18ebbf99e952f
hy <recul12@naver.com>
2020-06-15 09:06:02 +0900

    마감/진행 분리페이지 구현
    데이터를 받아와 삼항연산자로 만드려고 했으나 지속 에러로 모집 시작일+daycount와 현재 시간의 차수를 구해 음수면 마감, 양수면 진행하는 두가지 카드로 나누어 각 row에 들어가는 것으로 구현

--- client/src/components/views/LandingPage/Category/LandingMFCPage.js
+++ client/src/components/views/LandingPage/Category/LandingMFCPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
-import { Card, Avatar, Col, Typography, Row } from 'antd';
+import { Card, Avatar, Col, Typography, Row, Button } from 'antd';
 import axios from 'axios';
 import moment from 'moment';
 const { Title } = Typography;
@@ -9,7 +9,8 @@ const { Meta } = Card;
 function LandingMFCPage() {
 
     const [Posts, setPosts] = useState([])
-
+    var image = 'https://static.wadiz.kr/main/media/img-fundingopen-pc@2x.3311937d.jpg';
+    var mainImg = 'https://cdn.wadiz.kr/ft/images/green001/2020/0605/20200605142924749_2233.jpg/wadiz/optimize/';
     const CategoryVariable = {
         category: 3
     }
@@ -35,6 +36,8 @@ function LandingMFCPage() {
 
         function getPercent() {
             var peo = 100 / posts.people;
+            if (posts.joinPeople) {
+                peo = peo * posts.joinPeople}
             if (posts.in) {
                 peo = peo * posts.in
             } else {
@@ -43,13 +46,30 @@ function LandingMFCPage() {
             peo = peo + '%'
             return peo;
         }
-        return <Col lg={6} md={8} xs={24} style={{ marginBottom: '30px' }}>
-            <div style={{ position: 'relative', margin: '0px 15px', height: '150px', overflow:'hidden', border:'1px solid rgba(0,0,0,.2)', borderRadius:'10px 10px'}}>
-                <a href={`/post/${posts._id}`} >
-                    <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${posts.filePath}`} />
-                    
-                </a>
+        var finOrNot = posts.fin;
+        var sdate = posts.startday;
+        var thistime = moment();
+        var cdate = moment(posts.startday).add(posts.daycount, "days");
+        console.log('일 차이: ', moment.duration(cdate.diff(thistime)).asDays());
+        console.log('시간 차이: ', moment.duration(cdate.diff(thistime)).asHours());
+        var resultDate = moment.duration(cdate.diff(thistime)).asDays();
+        posts.resultDate=resultDate
+        if(resultDate>0){
+            return
+        }
+        return <Col lg={6} md={8} xs={24} style={{ marginBottom: '40px' }}>
+            <a href={`/post/${posts._id}`} >
+            <div style={{ position: 'relative', margin: '0px 10px', height: '150px', overflow: 'hidden', border: '1px solid rgba(0,0,0,.2)', borderRadius: '10px 10px' }}>
+                
+                   { finOrNot
+                    ?<div style={{ width: '100%',height: '150px',background:'darkgray'}}><img style={{ width: '100%',filter:'brightness(30%)' }} alt="thumbnail" src={`http://localhost:5000/${posts.filePath}`} />
+                    <p style={{position:'absolute', top:"50%",left:'50%',transform:'translate(-50%,-50%)',fontSize:'22px',color:'white',textAlign:'center',fontWeight:'600'}}>마감된<br/>프로젝트</p>
+                    </div>
+                    :<img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${posts.filePath}`} />}
+
+                   
             </div><br />
+            
             <Meta
                 avatar={
                     <Avatar src={posts.writer.image} />
@@ -64,13 +84,75 @@ function LandingMFCPage() {
             <div style={{ marginTop: '10px', marginLeft: '23px', width: '65%', height: '10px', display: 'inline-block', border: '1px solid gray', borderRadius: '10px 10px', overflow: 'hidden' }}>
                 <div className='bar' name='bar' id="bar" style={{ float: 'left', width: `${person}`, height: '10px', display: 'inline', border: '1px solid gray', borderRadius: '10px 10px', background: 'red' }}></div>
             </div>
-            <span> {posts.in ? posts.in : `0`}/{posts.people ? (posts.people + `명`) : `∞명`}</span>
+            <span> {posts.joinPeople ? posts.joinPeople : `0`}/{posts.people ? (posts.people + `명`) : `∞명`}</span>
             <br />
+            </a>
 
         </Col>
 
     })
+    const renderCards2 = Posts.map((posts, index) => {
 
+        // var minutes = Math.floor(post.duration / 60);
+        // var seconds = Math.floor(post.duration - minutes * 60);
+        var person = getPercent();
+
+        function getPercent() {
+            var peo = 100 / posts.people;
+            if (posts.in) {
+                peo = peo * posts.in
+            } else {
+                peo = peo * 0
+            }
+            peo = peo + '%'
+            return peo;
+        }
+        var finOrNot = posts.fin;
+        var sdate = posts.startday;
+        var thistime = moment();
+        var cdate = moment(posts.startday).add(posts.daycount, "days");
+        console.log('일 차이: ', moment.duration(cdate.diff(thistime)).asDays());
+        console.log('시간 차이: ', moment.duration(cdate.diff(thistime)).asHours());
+        var resultDate = moment.duration(cdate.diff(thistime)).asDays();
+        
+        if(resultDate<=0){
+            return
+        }
+        
+        return <Col lg={6} md={8} xs={24} style={{ marginBottom: '40px' }}>
+            <a href={`/post/${posts._id}`} >
+            <div style={{ position: 'relative', margin: '0px 10px', height: '150px', overflow: 'hidden', border: '1px solid rgba(0,0,0,.2)', borderRadius: '10px 10px' }}>
+                
+                   { finOrNot
+                    ?<div style={{ width: '100%',height: '150px',background:'darkgray'}}><img style={{ width: '100%',filter:'brightness(30%)' }} alt="thumbnail" src={`http://localhost:5000/${posts.filePath}`} />
+                    <p style={{position:'absolute', top:"50%",left:'50%',transform:'translate(-50%,-50%)',fontSize:'22px',color:'white',textAlign:'center',fontWeight:'600'}}>마감된<br/>프로젝트</p>
+                    </div>
+                    :<img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${posts.filePath}`} />}
+
+                   
+            </div><br />
+            
+            <Meta
+                avatar={
+                    <Avatar src={posts.writer.image} />
+                }
+                title={posts.title}
+            />
+            <span>{posts.writer.name} </span><br />
+
+            <span style={{ marginLeft: '3rem' }}> {posts.views} views</span>
+            - <span> {moment(posts.createdAt).format("MMM Do YY")} </span><br />
+
+            <div style={{ marginTop: '10px', marginLeft: '23px', width: '65%', height: '10px', display: 'inline-block', border: '1px solid gray', borderRadius: '10px 10px', overflow: 'hidden' }}>
+                <div className='bar' name='bar' id="bar" style={{ float: 'left', width: `${person}`, height: '10px', display: 'inline', border: '1px solid gray', borderRadius: '10px 10px', background: 'red' }}></div>
+            </div>
+            <span> {posts.joinPeople ? posts.joinPeople : `0`}/{posts.people ? (posts.people + `명`) : `∞명`}</span>
+            <br />
+            </a>
+
+        </Col>
+
+    })
 
     return (<div style={{width:'100%', overflow:'hidden'}}>
         <img id='landingImg' src="https://cdn.wadiz.kr/ft/images/green001/2020/0605/20200605142924749_2233.jpg/wadiz/optimize/" ></img>
@@ -86,11 +168,22 @@ function LandingMFCPage() {
 
             <Title level={2} style={{marginTop:'2rem'}}> MFC 프로젝트 </Title>
             <hr />
+            
 
-            <Row gutter={16}>
-                {renderCards}
-            </Row>
+            <Title level={4} style={{marginTop:'2rem'}}> 진행중 </Title>
+            <Row gutter={16}>{renderCards}</Row>
+            <hr />
+            
+            <Title level={4} style={{marginTop:'2rem'}}> 마감 </Title>
+            <Row gutter={16}>{renderCards2}</Row>
+            <hr />
         </div>
+        <section style={{backgroundImage: `url(${image})`, padding:'60px 0px', textAlign:'center', height:'280px'}}><a href="/upload">
+            <h1 style={{color:'white', fontSize:'32px', margin:'0px'}}>소개매칭에서 프로젝트 오픈하기</h1>
+            <p style={{color:'white', fontSize:'15px'}}>당신의 아이디어를 소개하고 개발팀을 꾸려보세요</p>
+            <Button style={{backgroundColor:'#00c4c4', color:'white',height:'56px', fontSize:'21px',padding:'0 32px', border:'0px solid white', borderRadius:'5px 5px'}}>바로가기&nbsp;<i aria-hidden="true"></i></Button>
+        </a>
+        </section>
         </div>
     )
 }
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
-import { Card, Avatar, Col, Typography, Row,Button } from 'antd';
+import { Card, Avatar, Col, Typography, Row, Button } from 'antd';
 import axios from 'axios';
 import moment from 'moment';
 const { Title } = Typography;
 const { Meta } = Card;
 
 function LandingPage() {
-
+    
     const [Posts, setPosts] = useState([])
     var image = 'https://static.wadiz.kr/main/media/img-fundingopen-pc@2x.3311937d.jpg';
     var mainImg = 'https://cdn.wadiz.kr/ft/images/green001/2020/0605/20200605142924749_2233.jpg/wadiz/optimize/';
-
     useEffect(() => {
         axios.get('http://localhost:5000/api/post/getPosts')
             .then(response => {
@@ -22,20 +21,23 @@ function LandingPage() {
                     alert('Failed to get Posts')
                 }
             })
+        
+
     }, [])
 
-    
+
     const renderCards = Posts.map((posts, index) => {
 
         // var minutes = Math.floor(post.duration / 60);
         // var seconds = Math.floor(post.duration - minutes * 60);
         var person = getPercent();
-        
 
         function getPercent() {
             var peo = 100 / posts.people;
             if (posts.joinPeople) {
-                peo = peo * posts.joinPeople
+                peo = peo * posts.joinPeople}
+            if (posts.in) {
+                peo = peo * posts.in
             } else {
                 peo = peo * 0
             }
@@ -43,7 +45,16 @@ function LandingPage() {
             return peo;
         }
         var finOrNot = posts.fin;
-        
+        var sdate = posts.startday;
+        var thistime = moment();
+        var cdate = moment(posts.startday).add(posts.daycount, "days");
+        console.log('일 차이: ', moment.duration(cdate.diff(thistime)).asDays());
+        console.log('시간 차이: ', moment.duration(cdate.diff(thistime)).asHours());
+        var resultDate = moment.duration(cdate.diff(thistime)).asDays();
+        posts.resultDate=resultDate
+        if(resultDate>0){
+            return
+        }
         
         return <Col lg={6} md={8} xs={24} style={{ marginBottom: '40px' }}>
             <a href={`/post/${posts._id}`} >
@@ -57,6 +68,7 @@ function LandingPage() {
 
                    
             </div><br />
+            
             <Meta
                 avatar={
                     <Avatar src={posts.writer.image} />
@@ -79,12 +91,75 @@ function LandingPage() {
 
     })
 
+    const renderCards2 = Posts.map((posts, index) => {
 
-    return (<div style={{ width: '100%', overflow: 'hidden' }}>
+        // var minutes = Math.floor(post.duration / 60);
+        // var seconds = Math.floor(post.duration - minutes * 60);
+        var person = getPercent();
+
+        function getPercent() {
+            var peo = 100 / posts.people;
+            if (posts.joinPeople) {
+                peo = peo * posts.joinPeople}
+            if (posts.in) {
+                peo = peo * posts.in
+            } else {
+                peo = peo * 0
+            }
+            peo = peo + '%'
+            return peo;
+        }
+        var finOrNot = posts.fin;
+        var sdate = posts.startday;
+        var thistime = moment();
+        var cdate = moment(posts.startday).add(posts.daycount, "days");
+        console.log('일 차이: ', moment.duration(cdate.diff(thistime)).asDays());
+        console.log('시간 차이: ', moment.duration(cdate.diff(thistime)).asHours());
+        var resultDate = moment.duration(cdate.diff(thistime)).asDays();
         
-        <img id='landingImg' src="https://cdn.wadiz.kr/ft/images/green001/2020/0605/20200605142924749_2233.jpg/wadiz/optimize/" 
-        style={{ textAlign:'center', minHeight:'280px', resize:'both', width:'100%', borderRadius:'10px 10px'}}></img>
-        <div id='renderZone' style={{ width: '85%', margin: '3rem auto' }}>
+        if(resultDate<=0){
+            return
+        }
+        
+        return <Col lg={6} md={8} xs={24} style={{ marginBottom: '40px' }}>
+            <a href={`/post/${posts._id}`} >
+            <div style={{ position: 'relative', margin: '0px 10px', height: '150px', overflow: 'hidden', border: '1px solid rgba(0,0,0,.2)', borderRadius: '10px 10px' }}>
+                
+                   { finOrNot
+                    ?<div style={{ width: '100%',height: '150px',background:'darkgray'}}><img style={{ width: '100%',filter:'brightness(30%)' }} alt="thumbnail" src={`http://localhost:5000/${posts.filePath}`} />
+                    <p style={{position:'absolute', top:"50%",left:'50%',transform:'translate(-50%,-50%)',fontSize:'22px',color:'white',textAlign:'center',fontWeight:'600'}}>마감된<br/>프로젝트</p>
+                    </div>
+                    :<img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${posts.filePath}`} />}
+
+                   
+            </div><br />
+            
+            <Meta
+                avatar={
+                    <Avatar src={posts.writer.image} />
+                }
+                title={posts.title}
+            />
+            <span>{posts.writer.name} </span><br />
+
+            <span style={{ marginLeft: '3rem' }}> {posts.views} views</span>
+            - <span> {moment(posts.createdAt).format("MMM Do YY")} </span><br />
+
+            <div style={{ marginTop: '10px', marginLeft: '23px', width: '65%', height: '10px', display: 'inline-block', border: '1px solid gray', borderRadius: '10px 10px', overflow: 'hidden' }}>
+                <div className='bar' name='bar' id="bar" style={{ float: 'left', width: `${person}`, height: '10px', display: 'inline', border: '1px solid gray', borderRadius: '10px 10px', background: 'red' }}></div>
+            </div>
+            <span> {posts.joinPeople ? posts.joinPeople : `0`}/{posts.people ? (posts.people + `명`) : `∞명`}</span>
+            <br />
+            </a>
+
+        </Col>
+
+    })
+    
+    
+    return (<div style={{width:'100%', overflow:'hidden'}}>
+        <img id='landingImg' src="https://cdn.wadiz.kr/ft/images/green001/2020/0605/20200605142924749_2233.jpg/wadiz/optimize/" ></img>
+        <div id='renderZone'style={{ width: '85%', margin: '3rem auto' }}>
 
             <a href='/'><button>전체보기</button></a>
             <a href='/web'><button>웹 App</button></a>
@@ -94,21 +169,27 @@ function LandingPage() {
             <a href='/game'><button>Game</button></a>
             <a href='/normal'><button>일반/기타</button></a>
 
-            <Title level={2} style={{ marginTop: '2rem' }}> 전체보기 </Title>
+            <Title level={2} style={{marginTop:'2rem'}}> 전체보기 </Title>
             <hr />
+            
 
-            <Row gutter={16}>
-                {renderCards}
-            </Row>
+            <Title level={4} style={{marginTop:'2rem'}}> 진행중 </Title>
+            <Row gutter={16}>{renderCards}</Row>
+            <hr />
+            
+            <Title level={4} style={{marginTop:'2rem'}}> 마감 </Title>
+            <Row gutter={16}>{renderCards2}</Row>
+            <hr />
+            
+            
         </div>
-
         <section style={{backgroundImage: `url(${image})`, padding:'60px 0px', textAlign:'center', height:'280px'}}><a href="/upload">
-            <h1 style={{color:'white', fontSize:'32px', margin:'0px'}}>개매칭에서 프로젝트 오픈하기</h1>
+            <h1 style={{color:'white', fontSize:'32px', margin:'0px'}}>소개매칭에서 프로젝트 오픈하기</h1>
             <p style={{color:'white', fontSize:'15px'}}>당신의 아이디어를 소개하고 개발팀을 꾸려보세요</p>
             <Button style={{backgroundColor:'#00c4c4', color:'white',height:'56px', fontSize:'21px',padding:'0 32px', border:'0px solid white', borderRadius:'5px 5px'}}>바로가기&nbsp;<i aria-hidden="true"></i></Button>
         </a>
         </section>
-    </div>
+        </div>
     )
 }
 
